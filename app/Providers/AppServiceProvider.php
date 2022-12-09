13c26f3a0ebc704018b28a6788fde62f08654fc3
Ramakant Gangwar <gangwar.ramakant@gmail.com>
2022-12-09 15:14:17 +0530

    Fix https issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -78,5 +78,13 @@ class AppServiceProvider extends ServiceProvider
         $this->registerViewComposers();
 
         $router->aliasMiddleware('tenant-admin.user', TenantVoyagerAdminMiddleware::class);
+
+        \URL::forceRootUrl(\Config::get('app.url'));
+        // And this if you wanna handle https URL scheme
+        // It's not usefull for http://www.example.com, it's just to make it more independant from the constant value
+        if (\Str::contains(\Config::get('app.url'), 'https://')) {
+            \URL::forceScheme('https');
+            //use \URL:forceSchema('https') if you use laravel < 5.4
+        }
     }
 }
